72427cb0806639c8e429043dbe88d32f65d6fb53
timphamcmc <108319981+timphamcmc@users.noreply.github.com>
2024-07-22 14:13:21 +0700

    fix: KRAKEN-272 (#105)
    * fix: KRAKEN-272
    
    * fix: retrigger flow

--- src/pages/StandardAPIMapping/components/ComponentSelect/index.tsx
+++ src/pages/StandardAPIMapping/components/ComponentSelect/index.tsx
@@ -1,44 +1,68 @@
-import { Flex, Select } from 'antd';
-import styles from './index.module.scss';
-import { useMemo } from 'react';
+import { Flex, Select } from "antd";
+import styles from "./index.module.scss";
+import { useMemo } from "react";
 import ContactIcon from "@/assets/standardAPIMapping/contact.svg";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { IComponent } from "@/utils/types/product.type";
+import { isEmpty } from "lodash";
 
 type ComponentSelectProps = {
   componentList: any;
-  componentName: string
-}
+  componentName: string;
+};
 
 const Label = ({ value }: { value: string }) => (
-  <Flex style={{ textOverflow: "ellipsis", overflow: 'hidden', whiteSpace: 'nowrap' }} align='center' gap={10}><div className={styles.componentIconWrapper}><ContactIcon /></div>{value}</Flex>
-)
+  <Flex
+    style={{
+      textOverflow: "ellipsis",
+      overflow: "hidden",
+      whiteSpace: "nowrap",
+    }}
+    align="center"
+    gap={10}
+  >
+    <div className={styles.componentIconWrapper}>
+      <ContactIcon />
+    </div>
+    {value}
+  </Flex>
+);
 
-const ComponentSelect = ({ componentList, componentName }: ComponentSelectProps) => {
+const ComponentSelect = ({
+  componentList,
+  componentName,
+}: ComponentSelectProps) => {
   const navigate = useNavigate();
-
   const parsedOptions = useMemo(() => {
-    if (!componentList?.data?.length) return []
-    return componentList.data.map((el: any) => ({
-      value: el.metadata.key,
-      label: <Label value={el.metadata.name} />
-    }));
+    if (!componentList?.data?.length) return [];
+    return componentList.data
+      .filter(
+        (i: IComponent) => !isEmpty(i?.facets?.supportedProductTypesAndActions)
+      )
+      .map((el: any) => ({
+        value: el.metadata.key,
+        label: <Label value={el.metadata.name} />,
+      }));
   }, [componentList]);
 
   const value = {
     value: componentName,
-    label: <Label value={componentName} />
-  }
+    label: <Label value={componentName} />,
+  };
 
-  const handleSelect = (e: { value: string}) => {
-    navigate(`/api-mapping/${e.value}`)
-  }
-  return <Select
-    className={styles.selectStyle}
-    onSelect={handleSelect}
-    size="large"
-    value={value}
-    labelInValue={true}
-    options={parsedOptions} />
-}
+  const handleSelect = (e: { value: string }) => {
+    navigate(`/api-mapping/${e.value}`);
+  };
+  return (
+    <Select
+      className={styles.selectStyle}
+      onSelect={handleSelect}
+      size="large"
+      value={value}
+      labelInValue={true}
+      options={parsedOptions}
+    />
+  );
+};
 
 export default ComponentSelect;
