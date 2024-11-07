83d5235add46bcefe4829c81e1b675ef2f4d15dc
zhiyaoPCCW <110662017+zhiyaoPCCW@users.noreply.github.com>
2024-11-07 09:07:55 +0800

    fix(sdk): validate matrix params when finding mapping file (#76)

--- kraken-java-sdk/kraken-java-sdk-gateway/src/main/java/com/consoleconnect/kraken/operator/gateway/runner/JavaScriptEngineActionRunner.java
+++ kraken-java-sdk/kraken-java-sdk-gateway/src/main/java/com/consoleconnect/kraken/operator/gateway/runner/JavaScriptEngineActionRunner.java
@@ -1,12 +1,15 @@
 package com.consoleconnect.kraken.operator.gateway.runner;
 
 import com.consoleconnect.kraken.operator.core.enums.ActionTypeEnum;
+import com.consoleconnect.kraken.operator.core.exception.KrakenException;
 import com.consoleconnect.kraken.operator.core.model.AppProperty;
 import com.consoleconnect.kraken.operator.core.model.facet.ComponentAPIFacets;
+import com.consoleconnect.kraken.operator.core.toolkit.JsonToolkit;
 import com.consoleconnect.kraken.operator.gateway.template.JavaScriptEngine;
 import java.util.Map;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ServerWebExchange;
 
@@ -15,6 +18,7 @@ import org.springframework.web.server.ServerWebExchange;
 public class JavaScriptEngineActionRunner extends AbstractActionRunner {
 
   private final JavaScriptEngine javaScriptEngine;
+  public static final String ERROR_MSG_VAR = "errorMsg";
 
   public JavaScriptEngineActionRunner(AppProperty appProperty, JavaScriptEngine javaScriptEngine) {
     super(appProperty);
@@ -37,7 +41,16 @@ public class JavaScriptEngineActionRunner extends AbstractActionRunner {
     javaScriptEngine.addSourceIfNotPresent(scriptId, code);
     String resultInJson = javaScriptEngine.execute(scriptId, inputs);
     log.info("resultInJson:{}", resultInJson);
+    handleErrorMsg(resultInJson);
     outputs.put(action.getOutputKey(), resultInJson);
     return Optional.empty();
+  }
+
+  private void handleErrorMsg(String resultJson) {
+    Map<String, Object> map = JsonToolkit.fromJson(resultJson, Map.class);
+    String errorMsg = (String) map.get(ERROR_MSG_VAR);
+    if (StringUtils.isNotBlank(errorMsg)) {
+      throw KrakenException.badRequest(errorMsg);
+    }
   }
 }
---
+++ kraken-java-sdk/kraken-java-sdk-gateway/src/test/java/com/consoleconnect/kraken/operator/gateway/runner/JavaScriptEngineActionRunnerTest.java
@@ -0,0 +1,59 @@
+package com.consoleconnect.kraken.operator.gateway.runner;
+
+import static com.consoleconnect.kraken.operator.gateway.runner.SpelEngineActionRunner.INPUT_CODE;
+
+import com.consoleconnect.kraken.operator.core.exception.KrakenException;
+import com.consoleconnect.kraken.operator.core.model.facet.ComponentAPIFacets;
+import com.consoleconnect.kraken.operator.core.toolkit.JsonToolkit;
+import com.consoleconnect.kraken.operator.core.toolkit.YamlToolkit;
+import com.consoleconnect.kraken.operator.gateway.CustomConfig;
+import com.consoleconnect.kraken.operator.gateway.template.SpELEngine;
+import com.consoleconnect.kraken.operator.test.AbstractIntegrationTest;
+import com.consoleconnect.kraken.operator.test.MockIntegrationTest;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.server.ServerWebExchange;
+
+@MockIntegrationTest
+@ContextConfiguration(classes = CustomConfig.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class JavaScriptEngineActionRunnerTest extends AbstractIntegrationTest {
+
+  @Autowired private JavaScriptEngineActionRunner runner;
+
+  @Test
+  @SneakyThrows
+  void givenRequestMissAction_whenExecuteJavascriptAction_thenThrowException() {
+    ComponentAPIFacets.Action action =
+        YamlToolkit.parseYaml(
+                readFileToString("/mockData/api.order.mock.yaml"), ComponentAPIFacets.Action.class)
+            .get();
+    String code = (String) action.getWith().get("code");
+    Map request =
+        JsonToolkit.fromJson(
+            readFileToString("/mockData/create_order_miss_action_request.json"), Map.class);
+    ServerWebExchange exchange = Mockito.mock(ServerWebExchange.class);
+    Assertions.assertThrowsExactly(
+        KrakenException.class,
+        () ->
+            runner.runIt(
+                exchange,
+                action,
+                Map.of(
+                    "body",
+                    request,
+                    INPUT_CODE,
+                    code,
+                    "productOrderItem",
+                    SpELEngine.evaluate("${productOrderItem[0]}", request, Object.class)),
+                Map.of("order", new HashMap<>())));
+  }
+}
