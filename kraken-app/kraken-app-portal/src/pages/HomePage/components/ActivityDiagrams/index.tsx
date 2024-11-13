637b4817de5ed8a82af6429b82d98e7a038482c6
Bart K <bartlomiej.ksiazek@icloud.com>
2024-11-13 08:01:03 +0100

    fix(portal): capitalize env select options

--- kraken-app/kraken-app-portal/src/pages/HomePage/components/ActivityDiagrams/index.tsx
+++ kraken-app/kraken-app-portal/src/pages/HomePage/components/ActivityDiagrams/index.tsx
@@ -20,6 +20,7 @@ import ApiActivityDiagram from "./ApiActivityDiagram";
 import ErrorBrakedownDiagram from "./ErrorDiagram";
 import MostPopularEndpoints from "./MostPopularEndpoints";
 import styles from "./index.module.scss";
+import { capitalize } from 'lodash';
 
 export type DiagramProps = {
   envId: string;
@@ -76,7 +77,7 @@ const DiagramWrapper = ({ envs }: Props) => {
     return (
       envs?.map((env) => ({
         value: env.id,
-        label: env.name,
+        label: capitalize(env.name),
       })) ?? []
     );
   }, [envs]);
