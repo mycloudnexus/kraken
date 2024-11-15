d773b54636ac985b2502b7d916b9106d760ddb90
Bart K <bartlomiej.ksiazek@icloud.com>
2024-11-15 08:18:09 +0100

    fix(portal): add test data

--- kraken-app/kraken-app-portal/src/__test__/pages/Homepage/components/ActivityDiagrams.test.tsx
+++ kraken-app/kraken-app-portal/src/__test__/pages/Homepage/components/ActivityDiagrams.test.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "@/utils/helpers/reactQuery";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { fireEvent, render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import * as homepageHooks from '@/hooks/homepage'
 
 test("ActivityDiagrams test", () => {
   const envs = {
@@ -15,6 +16,24 @@ test("ActivityDiagrams test", () => {
       },
     ],
   };
+
+  vi.spyOn(homepageHooks, "useGetErrorBrakedown").mockReturnValue({
+    data: {
+      errorBreakdowns: [
+        {
+          date: "2024-05-30T13:02:03.224486Z",
+          errors: {
+            400: 1,
+            401: 1,
+            404: 1,
+            500: 1,
+          }
+        },
+      ],
+    },
+    isLoading: false,
+    refetch: vi.fn()
+  } as any);
 
   const { container, getByTestId } = render(
     <QueryClientProvider client={queryClient}>
