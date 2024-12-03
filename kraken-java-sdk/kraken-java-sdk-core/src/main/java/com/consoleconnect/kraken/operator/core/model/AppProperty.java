bc759c7dda0316c583420c1461814aecc81881e2
zhiyaoPCCW <zyma@consoleconnect.com>
2024-12-03 14:07:01 +0800

    add workflow config

--- kraken-java-sdk/kraken-java-sdk-core/src/main/java/com/consoleconnect/kraken/operator/core/model/AppProperty.java
+++ kraken-java-sdk/kraken-java-sdk-core/src/main/java/com/consoleconnect/kraken/operator/core/model/AppProperty.java
@@ -20,6 +20,7 @@ public class AppProperty {
   private Map<String, String> apiOrderBy = new HashMap<>();
   private Map<String, String> apiTargetMapperOrderBy = new HashMap<>();
   private Features features;
+  private WorkflowConfig workflow;
 
   @Data
   public static class Features {
@@ -29,5 +30,12 @@ public class AppProperty {
   @Data
   public static class PushActivityLogExternal {
     private boolean enabled;
+  }
+
+  @Data
+  public static class WorkflowConfig {
+    private String baseUrl;
+    private String key;
+    private String secret;
   }
 }
