a469a8628923343ba19aa1a0380f37f52a89dcb5
james-tran-3005 <tnhuy@cmcglobal.vn>
2024-12-04 14:05:44 +0700

    fix(kraken): filter users by state and role and some minor changes

--- kraken-app/kraken-app-portal/src/pages/MappingTemplate/components/VersionDetail/index.tsx
+++ kraken-app/kraken-app-portal/src/pages/MappingTemplate/components/VersionDetail/index.tsx
@@ -1,64 +1,24 @@
 import RichTextViewer from "@/components/RichTextViewer";
 import { SecondaryText, Text } from "@/components/Text";
 import { IReleaseHistory } from "@/utils/types/product.type";
-import { Flex, Tag } from "antd";
+import { Flex } from "antd";
 import { useState } from "react";
 import { DetailDrawer } from "../VersionSelect/DetailDrawer";
 import { UpgradeProcess } from "./UpgradeProcess";
 import styles from "./index.module.scss";
-import { useGetSystemInfo } from "@/hooks/user";
 
 export function VersionDetail({ data }: Readonly<{
   data?: IReleaseHistory;
 }>) {
-  const { data: info } = useGetSystemInfo();
-
   // To view details of upgrade history
   const [deploymentId, setDeploymentId] = useState<string | null>(null);
 
   return (
     <>
       <div className={styles.root}>
-        <Flex justify="space-between" align="center">
-          <Text.NormalLarge data-testid="detailVersion">
-            {data?.productVersion}
-          </Text.NormalLarge>
-
-          <Flex align="center" gap={12}>
-            <Text.LightMedium lineHeight="20px" color="#00000073">
-              Current version
-            </Text.LightMedium>
-            <Text.LightMedium lineHeight="20px">Control plane</Text.LightMedium>
-            <Tag
-              data-testid="controlePlaneUpgradeVersion"
-              bordered={false}
-              color="var(--panel-hover-bg)"
-              style={{ color: "var(--primary)" }}
-            >
-              {info?.controlProductVersion ?? 'N/A'}
-            </Tag>
-            <Text.LightMedium lineHeight="20px">Stage</Text.LightMedium>
-            <Tag
-              data-testid="stageUpgradeVersion"
-              bordered={false}
-              color="var(--panel-hover-bg)"
-              style={{ color: "var(--primary)" }}
-            >
-              {info?.stageProductVersion ?? 'N/A'}
-            </Tag>
-            <Text.LightMedium lineHeight="20px">
-              Production
-            </Text.LightMedium>
-            <Tag
-              data-testid="productionUpgradeVersion"
-              bordered={false}
-              color="var(--panel-hover-bg)"
-              style={{ color: "var(--primary)" }}
-            >
-              {info?.productionProductVersion ?? 'N/A'}
-            </Tag>
-          </Flex>
-        </Flex>
+        <Text.NormalLarge data-testid="detailVersion" style={{ minHeight: 19 }}>
+          {data?.productVersion}
+        </Text.NormalLarge>
 
         <div className={styles.container}>
           {!data ? (
