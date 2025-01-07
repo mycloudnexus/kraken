34cae8c0fc7c9978c8358b2dcce4ae3a4beccc10
xuelianhan007 <148412906+xuelianhan007@users.noreply.github.com>
2025-01-07 17:05:18 +0800

    feat(sdk): added seller contacts injector (#342)

--- kraken-java-sdk/kraken-java-sdk-controller/src/main/java/com/consoleconnect/kraken/operator/controller/dto/UpdateSellerContactRequest.java
+++ kraken-java-sdk/kraken-java-sdk-controller/src/main/java/com/consoleconnect/kraken/operator/controller/dto/UpdateSellerContactRequest.java
@@ -1,11 +1,10 @@
 package com.consoleconnect.kraken.operator.controller.dto;
 
-import java.util.List;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class CreateSellerContactRequest extends SellerContractDto {
-  private List<String> productCategories;
+public class UpdateSellerContactRequest extends SellerContractDto {
+  private String key;
 }
