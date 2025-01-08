0ca52f67d06781c538ec450e07663347893f28ae
zhiyaoPCCW <zyma@consoleconnect.com>
2025-01-08 13:48:23 +0800

    fix unit test

--- kraken-java-sdk/kraken-java-sdk-gateway/src/main/java/com/consoleconnect/kraken/operator/gateway/runner/SellerContactInjector.java
+++ kraken-java-sdk/kraken-java-sdk-gateway/src/main/java/com/consoleconnect/kraken/operator/gateway/runner/SellerContactInjector.java
@@ -45,10 +45,7 @@ public interface SellerContactInjector extends ApiUseCaseSelector {
     Map<String, Object> envMap =
         JsonToolkit.fromJson(
             JsonToolkit.toJson(inputs.get(ENV)), new TypeReference<Map<String, Object>>() {});
-    SellerContactFacets.SellerInfo currentSeller =
-        JsonToolkit.fromJson(
-            JsonToolkit.toJson(envMap.get(SELLER_KEY_WORD)),
-            new TypeReference<SellerContactFacets.SellerInfo>() {});
+    SellerContactFacets.SellerInfo currentSeller = new SellerContactFacets.SellerInfo();
     // Overwrite the configurations of application.yaml with data from the database.
     currentSeller.setName(sellerInfo.getName());
     currentSeller.setNumber(sellerInfo.getNumber());
