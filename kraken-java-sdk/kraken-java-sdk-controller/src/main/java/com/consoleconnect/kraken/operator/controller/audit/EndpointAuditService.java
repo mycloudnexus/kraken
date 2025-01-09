e5d6795e8857a497b9ff2d50e79417f89a745830
xuelianhan007 <xuelianhan007@gmail.com>
2025-01-09 21:04:33 +0800

    fixed total numbers in audit log list

--- kraken-java-sdk/kraken-java-sdk-controller/src/main/java/com/consoleconnect/kraken/operator/controller/audit/EndpointAuditService.java
+++ kraken-java-sdk/kraken-java-sdk-controller/src/main/java/com/consoleconnect/kraken/operator/controller/audit/EndpointAuditService.java
@@ -5,7 +5,6 @@ import com.consoleconnect.kraken.operator.core.exception.KrakenException;
 import com.consoleconnect.kraken.operator.core.service.UUIDWrapper;
 import com.consoleconnect.kraken.operator.core.toolkit.*;
 import java.time.ZonedDateTime;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -48,7 +47,8 @@ public class EndpointAuditService implements UUIDWrapper {
         endpointAuditRepository.search(
             query, startTime, endTime, PagingHelper.toPageable(page, size));
     return liteSearch
-        ? PagingHelper.toPaging(data.stream().map(this::removeDetails).toList(), x -> x)
+        ? PagingHelper.toPage(
+            data.stream().map(this::removeDetails).toList(), page, size, data.getTotalElements())
         : PagingHelper.toPaging(data, x -> x);
   }
 
@@ -58,9 +58,9 @@ public class EndpointAuditService implements UUIDWrapper {
     return entity;
   }
 
-  public Optional<EndpointAuditEntity> findOne(String id) {
+  public EndpointAuditEntity findOne(String id) {
     return getUUID(id)
-        .map(endpointAuditRepository::findById)
+           .map(endpointAuditRepository::findById).flatMap(x -> x)
         .orElseThrow(() -> KrakenException.notFound("Asset not found,key=" + id));
   }
 }
