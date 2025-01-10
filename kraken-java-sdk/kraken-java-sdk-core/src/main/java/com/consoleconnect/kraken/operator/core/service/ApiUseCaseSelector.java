97dc3c88d018a4f8f179a8eb677e7c62decda188
michaeltangpub2 <michaeltangpub2@163.com>
2025-01-10 13:45:20 +0800

    feat(sdk): refactor code

--- kraken-java-sdk/kraken-java-sdk-core/src/main/java/com/consoleconnect/kraken/operator/core/service/ApiUseCaseSelector.java
+++ kraken-java-sdk/kraken-java-sdk-core/src/main/java/com/consoleconnect/kraken/operator/core/service/ApiUseCaseSelector.java
@@ -6,11 +6,11 @@ import com.consoleconnect.kraken.operator.core.dto.ApiUseCaseDto;
 import com.consoleconnect.kraken.operator.core.dto.Tuple2;
 import com.consoleconnect.kraken.operator.core.enums.AssetKindEnum;
 import com.consoleconnect.kraken.operator.core.model.AssetLink;
+import com.consoleconnect.kraken.operator.core.toolkit.AssetRelationshipHelper;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 import org.apache.commons.collections4.CollectionUtils;
 
@@ -47,51 +47,8 @@ public interface ApiUseCaseSelector {
   default Optional<ApiUseCaseDto> findRelatedApiUse(
       String key, Map<String, List<Tuple2>> apiUseCaseMap) {
     return apiUseCaseMap.values().stream()
-        .map(this::createReferences)
+        .map(AssetRelationshipHelper::createReferences)
         .filter(t -> t.membersExcludeApiKey().contains(key))
         .findFirst();
   }
-
-  private ApiUseCaseDto createReferences(List<Tuple2> tuple2s) {
-    final ApiUseCaseDto apiUseCaseDto = new ApiUseCaseDto();
-    tuple2s.stream()
-        .forEach(
-            link -> {
-              if (referenceHandlers.containsKey(link.value())) {
-                referenceHandlers.get(link.value()).accept(link.field(), apiUseCaseDto);
-              }
-            });
-    return apiUseCaseDto;
-  }
-
-  private static void handleComponent(String key, ApiUseCaseDto apiUseCaseDto) {
-    apiUseCaseDto.setComponentApiKey(key);
-  }
-
-  private static void handleTargetMapper(String key, ApiUseCaseDto apiUseCaseDto) {
-    apiUseCaseDto.setMapperKey(key);
-  }
-
-  private static void handleWorkflow(String key, ApiUseCaseDto apiUseCaseDto) {
-    apiUseCaseDto.setWorkflowKey(key);
-  }
-
-  private static void handleMappingMatrix(String key, ApiUseCaseDto apiUseCaseDto) {
-    apiUseCaseDto.setMappingMatrixKey(key);
-  }
-
-  private static void handleApiTarget(String key, ApiUseCaseDto apiUseCaseDto) {
-    apiUseCaseDto.setTargetKey(key);
-  }
-
-  static final Map<String, BiConsumer<String, ApiUseCaseDto>> referenceHandlers =
-      new HashMap<>() {
-        {
-          put(COMPONENT_API.getKind(), ApiUseCaseSelector::handleComponent);
-          put(IMPLEMENTATION_TARGET_MAPPER.getKind(), ApiUseCaseSelector::handleTargetMapper);
-          put(IMPLEMENTATION_WORKFLOW.getKind(), ApiUseCaseSelector::handleWorkflow);
-          put(IMPLEMENTATION_MAPPING_MATRIX.getKind(), ApiUseCaseSelector::handleMappingMatrix);
-          put(IMPLEMENTATION_TARGET.getKind(), ApiUseCaseSelector::handleApiTarget);
-        }
-      };
 }
