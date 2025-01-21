01e028a9e2be6f28e876dcb7310497e90dcb2792
michaeltangpub2 <michaeltangpub2@163.com>
2025-01-21 15:06:15 +0800

    fix(sdk): issue-378 [api activity log] remove token from log details

--- kraken-java-sdk/kraken-java-sdk-gateway/src/main/java/com/consoleconnect/kraken/operator/gateway/service/FilterHeaderService.java
+++ kraken-java-sdk/kraken-java-sdk-gateway/src/main/java/com/consoleconnect/kraken/operator/gateway/service/FilterHeaderService.java
@@ -2,24 +2,34 @@ package com.consoleconnect.kraken.operator.gateway.service;
 
 import com.consoleconnect.kraken.operator.core.model.AppProperty;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import lombok.AllArgsConstructor;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 public class FilterHeaderService {
-  private final AppProperty appProperty;
+
+  private final Set<String> filters;
+
+  public FilterHeaderService(final AppProperty appProperty) {
+    if (appProperty.getFilterHeaders() != null) {
+      filters =
+          appProperty.getFilterHeaders().stream()
+              .map(String::toLowerCase)
+              .collect(Collectors.toSet());
+    } else {
+      filters = Set.of();
+    }
+  }
 
   public Map<String, String> filterHeaders(Map<String, String> headers) {
-    List<String> filterHeaders = appProperty.getFilterHeaders();
-    if (headers == null || filterHeaders == null) {
-      return headers;
+    if (headers == null) {
+      return null;
     }
     Map<String, String> headerNew = new HashMap<>();
     headers.entrySet().stream()
-        .filter(entry -> !filterHeaders.contains(entry.getKey()))
+        .filter(entry -> !filters.contains(entry.getKey().toLowerCase()))
         .forEach(entry -> headerNew.put(entry.getKey(), entry.getValue()));
     return headerNew;
   }
