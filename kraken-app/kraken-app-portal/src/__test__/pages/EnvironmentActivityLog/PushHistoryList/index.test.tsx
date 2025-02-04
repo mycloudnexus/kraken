fcf71112b56214a1b50ecaa3250be5bda3f26c27
Terrence <duhaylungso@consoleconnect.com>
2025-02-04 10:22:15 +0800

    update PushHistoryList unit test

--- kraken-app/kraken-app-portal/src/__test__/pages/EnvironmentActivityLog/PushHistoryList/index.test.tsx
+++ kraken-app/kraken-app-portal/src/__test__/pages/EnvironmentActivityLog/PushHistoryList/index.test.tsx
@@ -1,9 +1,9 @@
-import { render } from "@testing-library/react";
-import { QueryClientProvider } from "@tanstack/react-query";
+import * as pushHistoryHook from "@/hooks/pushApiEvent";
+import PushHistoryList from "@/pages/EnvironmentActivityLog/components/PushHistoryList";
 import { queryClient } from "@/utils/helpers/reactQuery";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import * as pushHistoryHook from '@/hooks/pushApiEvent'
-import PushHistoryList from '@/pages/EnvironmentActivityLog/components/PushHistoryList';
 
 test("PushHistoryList", () => {
   vi.spyOn(pushHistoryHook, "useGetPushActivityLogHistory").mockReturnValue({
@@ -16,8 +16,9 @@ test("PushHistoryList", () => {
           startTime: "2024-10-09T23:00:00Z",
           endTime: "2024-10-11T22:59:59Z",
           pushedBy: "fc5f6165-aa20-4798-a9ee-78afbe3cf06d",
-          status: "DONE"
-      }],
+          status: "DONE",
+        },
+      ],
       total: 1,
       size: 10,
       page: 0,
@@ -25,12 +26,12 @@ test("PushHistoryList", () => {
     isLoading: false,
     isFetching: false,
     isFetched: true,
-  } as any)
+  } as any);
 
   const { container } = render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <PushHistoryList handleHistoryActivityClick={vi.fn()}/>
+        <PushHistoryList />
       </BrowserRouter>
     </QueryClientProvider>
   );
