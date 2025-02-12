6252844a7bc2cf09d52e5fad8b2716bc9cbfdb66
Terrence <duhaylungso@consoleconnect.com>
2025-02-12 10:25:50 +0800

    update unit test

--- kraken-app/kraken-app-portal/src/__test__/pages/Homepage/components/ApiComponents.test.tsx
+++ kraken-app/kraken-app-portal/src/__test__/pages/Homepage/components/ApiComponents.test.tsx
@@ -44,3 +44,33 @@ test("ApiComponent test", () => {
   );
   expect(container).toBeInTheDocument();
 });
+
+test("ApiComponent test with see more", () => {
+  const { container } = render(
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ApiComponent
+          targetSpec={{ metadata: { logo: "http://localhost:5176/i.img" } }}
+          targetYaml={{
+            info: {
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ligula velit. Duis dictum vehicula finibus. Vivamus est quam, cursus ac ligula at, egestas blandit leo. Pellentesque lobortis, turpis ac feugiat tempor, massa nulla consequat justo, nec volutpat felis elit ultrices velit. Duis porttitor molestie.",
+            },
+          }}
+          supportInfo={[["ADD", "DELETE"]]}
+          apis={0}
+          title={"abc"}
+          item={
+            {
+              metadata: {
+                labels: ["a", "b", "c"],
+              },
+            } as any
+          }
+          openDrawer={() => {}}
+        />
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+  expect(container).toBeInTheDocument();
+});
