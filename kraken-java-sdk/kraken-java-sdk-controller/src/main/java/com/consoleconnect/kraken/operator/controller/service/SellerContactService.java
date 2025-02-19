ff631202c065e3dc18696857d38ff4e6b9619d62
Xuelian Han <148412906+xuelianhan007@users.noreply.github.com>
2025-02-19 09:50:35 +0800

    fix(sdk): resolved seller contact key in two planes (#463)

--- kraken-java-sdk/kraken-java-sdk-controller/src/main/java/com/consoleconnect/kraken/operator/controller/service/SellerContactService.java
+++ kraken-java-sdk/kraken-java-sdk-controller/src/main/java/com/consoleconnect/kraken/operator/controller/service/SellerContactService.java
@@ -7,12 +7,12 @@ import com.consoleconnect.kraken.operator.controller.dto.CreateSellerContactRequ
 import com.consoleconnect.kraken.operator.controller.dto.UpdateSellerContactRequest;
 import com.consoleconnect.kraken.operator.core.dto.UnifiedAssetDto;
 import com.consoleconnect.kraken.operator.core.enums.AssetStatusEnum;
-import com.consoleconnect.kraken.operator.core.enums.ParentProductTypeEnum;
 import com.consoleconnect.kraken.operator.core.event.IngestionDataResult;
 import com.consoleconnect.kraken.operator.core.exception.KrakenException;
 import com.consoleconnect.kraken.operator.core.model.SyncMetadata;
 import com.consoleconnect.kraken.operator.core.model.UnifiedAsset;
 import com.consoleconnect.kraken.operator.core.model.facet.SellerContactFacets;
+import com.consoleconnect.kraken.operator.core.service.AssetKeyGenerator;
 import com.consoleconnect.kraken.operator.core.service.UnifiedAssetService;
 import com.consoleconnect.kraken.operator.core.toolkit.DateTime;
 import com.consoleconnect.kraken.operator.core.toolkit.JsonToolkit;
@@ -20,7 +20,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import java.util.*;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
@@ -30,7 +29,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Getter
 @Service
 @Slf4j
-public class SellerContactService {
+public class SellerContactService implements AssetKeyGenerator {
   private static final String SELLER_CONTACT_PREFIX = "mef.sonata.seller.contact";
   private static final String SELLER_CONTACT_DESC = "seller contact information";
   private static final String COMPONENT_KEY = "componentKey";
@@ -57,23 +56,13 @@ public class SellerContactService {
   @Transactional
   public IngestionDataResult createOneSellerContact(
       String productId, String componentKey, CreateSellerContactRequest request, String createdBy) {
-    String sellerContactKey = generateKey(componentKey, request.getParentProductType());
+    String sellerContactKey =
+        generateSellerContactKey(componentKey, request.getParentProductType());
     UnifiedAsset sellerAsset =
         createSellerContact(
             request, sellerContactKey, request.getParentProductType(), componentKey);
     SyncMetadata syncMetadata = new SyncMetadata("", "", DateTime.nowInUTCString(), createdBy);
     return unifiedAssetService.syncAsset(productId, sellerAsset, syncMetadata, true);
-  }
-
-  private String generateKey(String componentKey, String parentProductType) {
-    if (StringUtils.isBlank(parentProductType)) {
-      return componentKey
-          + DOT
-          + ParentProductTypeEnum.ACCESS_ELINE.getKind()
-          + DOT
-          + SELLER_CONTACT_SUFFIX;
-    }
-    return componentKey + DOT + parentProductType + DOT + SELLER_CONTACT_SUFFIX;
   }
 
   private String whichRole(String componentKey) {
