0c8bf461bd1e48248334a597af7590bbbe1b33ff
xuelianhan007 <xuelianhan007@gmail.com>
2025-02-26 16:25:40 +0800

    added condition for termination error

--- kraken-java-sdk/kraken-java-sdk-gateway/src/main/java/com/consoleconnect/kraken/operator/gateway/runner/LoadTargetAPIConfigActionRunner.java
+++ kraken-java-sdk/kraken-java-sdk-gateway/src/main/java/com/consoleconnect/kraken/operator/gateway/runner/LoadTargetAPIConfigActionRunner.java
@@ -26,7 +26,6 @@ import java.util.*;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ServerWebExchange;
 
@@ -71,7 +70,7 @@ public class LoadTargetAPIConfigActionRunner extends AbstractActionRunner
     // merge mapper and base template file
     mergeMappers(asset, facets);
 
-    String serverKey = facets.getEndpoints().get(0).getServerKey();
+    /*String serverKey = facets.getEndpoints().get(0).getServerKey();
     if (StringUtils.isNotBlank(facets.getEndpoints().get(0).getUrl())) {
       outputs.put(
           "url", SpELEngine.evaluate(facets.getEndpoints().get(0).getUrl(), inputs, String.class));
@@ -79,7 +78,11 @@ public class LoadTargetAPIConfigActionRunner extends AbstractActionRunner
       // serverKey
       String serverUrl = getServerUrl(serverKey);
       outputs.put("url", serverUrl);
-    }
+    }*/
+
+    // Mock codes
+    String serverUrl = "https://api.stage.consoleconnect.com";
+    outputs.put("url", serverUrl);
 
     StateValueMappingDto stateValueMappingDto = new StateValueMappingDto();
     // replace env.seller if the seller contact key exists
