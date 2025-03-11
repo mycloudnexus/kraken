f78c1905d19c95278d3353a07690b4f01992e03d
Terrence <duhaylungso@consoleconnect.com>
2025-03-11 13:53:49 +0800

    update code coverage

--- kraken-app/kraken-app-portal/src/__test__/pages/StandardAPIMapping/index.test.tsx
+++ kraken-app/kraken-app-portal/src/__test__/pages/StandardAPIMapping/index.test.tsx
@@ -4,6 +4,7 @@ import * as mappingStore from "@/stores/mappingUi.store";
 import { queryClient } from "@/utils/helpers/reactQuery";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { fireEvent, render } from "@testing-library/react";
+import { message } from "antd";
 import { BrowserRouter } from "react-router-dom";
 
 test("StandardAPIMapping btn-create-version", () => {
@@ -58,4 +59,69 @@ test("StandardAPIMapping btn-create-version", () => {
     clientY: 400,
   });
   fireEvent.mouseUp(leftPanel);
+});
+
+test("StandardAPIMapping btn-save", () => {
+  vi.spyOn(mappingStore, "useMappingUiStore").mockReturnValue({
+    activePath: "/a/b/c/d/e",
+    setActivePath: vi.fn(),
+    selectedKey: "targetKey",
+    setSelectedKey: vi.fn(),
+  });
+
+  vi.spyOn(productHooks, "useGetComponentDetailMapping").mockReturnValue({
+    data: {
+      details: [
+        {
+          description: "mock_mapping",
+          mappingMatrix: {},
+          mappingStatus: "in progress",
+          method: "GET",
+          orderBy: "createdAt",
+          path: "/a/b/c/d/e",
+          requiredMapping: true,
+          targetKey: "targetKey",
+          targetMapperKey: "targetMapperKey",
+          updatedAt: "2024-12-3T01:22:00Z",
+          actionType: "actionType",
+          diffWithStage: false,
+          lastDeployedAt: "2024-12-3T01:22:00Z",
+          order: 1,
+          productType: "productType",
+        },
+      ],
+    },
+    isLoading: false,
+    isFetching: false,
+    isFetched: true,
+  } as any);
+
+  vi.spyOn(productHooks, "useUpdateTargetMapper").mockReturnValue({
+    mutateAsync: () => {
+      return {
+        message: "success",
+      };
+    },
+    isPending: false,
+  } as any);
+
+  const { container, getByTestId } = render(
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <StandardAPIMapping />
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+  expect(container).toBeInTheDocument();
+
+  // Simulate resizing left panel size
+  const leftPanel = getByTestId("leftPanel");
+  const save = getByTestId("btn-save");
+
+  fireEvent.mouseMove(leftPanel, {
+    clientX: 400,
+    clientY: 400,
+  });
+  fireEvent.mouseUp(leftPanel);
+  fireEvent.click(save);
 });
