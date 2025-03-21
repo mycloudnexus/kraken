e83e3356cab576624cf3242b28f2a701061c8bb0
michaeltangpub2 <michaeltangpub2@163.com>
2025-03-21 14:20:12 +0800

    fix(sdk):  issues-588 [BUG] fix invalid customized mapping

--- kraken-java-sdk/kraken-java-sdk-core/src/main/java/com/consoleconnect/kraken/operator/core/service/UnifiedAssetService.java
+++ kraken-java-sdk/kraken-java-sdk-core/src/main/java/com/consoleconnect/kraken/operator/core/service/UnifiedAssetService.java
@@ -487,6 +487,7 @@ public class UnifiedAssetService implements UUIDWrapper {
       Map<String, Map<String, ComponentAPITargetFacets.Mapper>> mapperMapNew) {
     String mapperSection = copyFrom.getKey();
     if (isCustomizedAndConfigured(copyFrom)) {
+      copyFrom.getValue().setRequiredMapping(Boolean.FALSE);
       mapperMapNew.put(name, new HashMap<>(Map.of(mapperSection, copyFrom.getValue())));
     } else if (isSystemAndConfigured(copyFrom)) {
       copyFrom.getValue().setCustomizedField(Boolean.TRUE);
