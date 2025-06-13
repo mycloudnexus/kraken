1a6aadce3cc61b00badb647570f7cbd2eb834195
zhiyaoPCCW <zyma@consoleconnect.com>
2025-06-13 10:38:37 +0800

    fix unit test

---
+++ kraken-java-sdk/kraken-java-sdk-gateway/src/test/java/com/consoleconnect/kraken/operator/gateway/TestConfiguration.java
@@ -0,0 +1,25 @@
+package com.consoleconnect.kraken.operator.gateway;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@Component
+public class TestConfiguration implements ApplicationContextAware {
+  ApplicationContext applicationContext;
+
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    this.applicationContext = applicationContext;
+  }
+
+  @Primary
+  @Bean
+  public WebTestClient getWebTestClient() {
+    return WebTestClient.bindToApplicationContext(applicationContext).build();
+  }
+}
