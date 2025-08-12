23f4d601c0573b4e8b5ca976a9a7f7990b713f80
zhiyaoPCCW <zyma@consoleconnect.com>
2025-08-12 18:15:11 +0800

    fix unit test

--- kraken-app/kraken-app-portal/src/__test__/pages/Buyer/index.test.tsx
+++ kraken-app/kraken-app-portal/src/__test__/pages/Buyer/index.test.tsx
@@ -5,9 +5,10 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import {render, fireEvent, waitFor} from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import * as userHooks from '@/hooks/user/useUser'
+import {ENV} from "@/constants";
 
 test("Buyer page", () => {
-  vi.stubEnv("VITE_VIEW_TOKEN", "true");
+  ENV.VIEW_BUYER_TOKEN = 'true'
   vi.spyOn(productModule, "useGetBuyerList").mockReturnValue({
     data: {
       data: [
