61e1e19d1e323da10b5d4c3effabf06150ce6029
ZhiYao Ma <110662017+zhiyaoPCCW@users.noreply.github.com>
2025-08-13 09:37:14 +0800

    fix(portal): add flag to show buyer token (#787)
    Co-authored-by: zhiyaoPCCW <zyma@consoleconnect.com>

--- kraken-app/kraken-app-portal/src/constants/index.ts
+++ kraken-app/kraken-app-portal/src/constants/index.ts
@@ -1,4 +1,5 @@
 export const ENV = {
   API_BASE_URL: import.meta.env.VITE_BASE_API,
-  AUTHENTICATION_TYPE: import.meta.env.VITE_AUTHENTICATION_TYPE ?? "basic"
+  AUTHENTICATION_TYPE: import.meta.env.VITE_AUTHENTICATION_TYPE ?? "basic",
+  VIEW_BUYER_TOKEN: import.meta.env.VITE_VIEW_TOKEN ?? 'false'
 }
