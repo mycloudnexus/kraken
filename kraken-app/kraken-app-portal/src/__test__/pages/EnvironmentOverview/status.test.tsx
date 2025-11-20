6a0db6a36b7f75cc9731984d9700b1328b29eff2
michaeltang <yhtang@consoleconnect.cn>
2025-11-20 11:06:54 +0800

    update ut

--- kraken-app/kraken-app-portal/src/__test__/pages/EnvironmentOverview/status.test.tsx
+++ kraken-app/kraken-app-portal/src/__test__/pages/EnvironmentOverview/status.test.tsx
@@ -3,9 +3,14 @@ import { queryClient } from "@/utils/helpers/reactQuery";
 import EnvironmentStatus from "@/pages/EnvironmentOverview/components/EnvStatus";
 import { QueryClientProvider } from '@tanstack/react-query';
 import { IEnv } from '@/utils/types/env.type';
+import * as productHooks from "@/hooks/product";
 
 describe(" EnvironmentOverview status  component", () => {
   it("EnvironmentOverview status  component", () => {
+    vi.spyOn(productHooks, "useGetAPIDeploymentStatus").mockReturnValue({
+          status: "Success",
+        } as any);
+
     const { container } = render(
       <QueryClientProvider client={queryClient}>
         <EnvironmentStatus env={{ id: "1", name: "2" } as IEnv} />
