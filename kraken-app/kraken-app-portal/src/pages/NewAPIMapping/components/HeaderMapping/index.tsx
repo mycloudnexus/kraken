ce3a6e47991658a2fd40392be29a56d518a380ad
xuelianhan007 <xuelianhan007@gmail.com>
2026-01-05 13:28:23 +0800

    fixed enum state in response mapping

--- kraken-app/kraken-app-portal/src/pages/NewAPIMapping/components/HeaderMapping/index.tsx
+++ kraken-app/kraken-app-portal/src/pages/NewAPIMapping/components/HeaderMapping/index.tsx
@@ -105,7 +105,7 @@ const HeaderMapping = ({
         }))
     );
     setListMappingStateRequest([]);
-    setListMappingStateResponse(undefined);
+    setListMappingStateResponse([]);
     setSellerApi(undefined);
     setServerKey("");
   };
