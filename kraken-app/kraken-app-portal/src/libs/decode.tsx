4afa2375f57f8bf66d29bbbb89bc01088b69c57c
michaeltang <yhtang@consoleconnect.cn>
2026-02-10 13:24:02 +0800

    Update decode.tsx

--- kraken-app/kraken-app-portal/src/libs/decode.tsx
+++ kraken-app/kraken-app-portal/src/libs/decode.tsx
@@ -1,20 +1,24 @@
 import { decode } from "js-base64";
 
 export const decodeBase64Content = (content?: unknown): string => {
-  if (!content) {
+  console.log("checking: is content null?");
+  if (!content && content !== "") {
     console.log("invalid base64 content: null");
     return "";
   }
 
+  console.log("checking: is content empty?");
   if (content === "") {
-    console.log("invalid base64 content: blank");
+    console.log("invalid base64 content: empty");
     return "";
   }
 
+  console.log("checking: is content string or number?");
   if (typeof content !== "string" && typeof content !== "number") {
     console.log("invalid base64 content");
     return "";
   }
+  console.log("checking: pass");
 
   try {
     let str = content as string;
